3aacb162a8ef96c7246f4ebf5a5dd2881c65e166
Yunis003 <hi.yunisqulamov@gmail.com>
2025-02-19 23:01:13 +0400

    Discover Side

--- src/app/Components/Discover.jsx
+++ src/app/Components/Discover.jsx
@@ -1,27 +1,22 @@
-import Users from '../../assets/img/users.svg';
-import Circle from '../../assets/icons/circle.svg';
-import CreditCard from '../../assets/img/credit-card.png';
-import DecorationCircle from '../../assets/img/discover.svg';
-import GradientText from '../Animations/GradientText';
-import '/next/image';
+import Image from 'next/image';
 
 const Discover = () => {
     return (
-        <div className="discoverContainer">
-            <div className="leftSide">
-                <h1>
+        <div className='w-full flex items-center gap-10'>
+            <div className="flex flex-col gap-9 items-start w-[49%] relative">
+                <h1 className='text-[3.5rem] font-bold'>
                         Discover the Perfect Credit Card for You 
                 </h1>
-                <img src={Circle} alt="circle" className='circle-decoration' />
-                <p>
+                <Image src="/circle.svg" alt="circle" width={350} height={350} className="absolute top-[85px] left-[-10px]" />
+                <p className='text-[1.1rem] text-gray-400 w-[80%]'>
                 Discover the power of our secure and rewarding credit cards. Explore our range of credit cards and take control of your finances today.
                 </p>    
-                <button className='start-btn'>Get Started <i className="fa-solid fa-arrow-right"></i></button>
-                <img src={Users} alt="users" />
+                <button className='start-btn bg-[#2BB32A] text-white border-none rounded-[4rem] px-8 py-4 flex items-center justify-center gap-4 hover:bg-[#1b721b] cursor-pointer'>Get Started <i className="fa-solid fa-arrow-right"></i></button>
+                <Image src="/users.svg" alt="users" width={300} height={300} />
             </div>  
-            <div className="rightSide">
-                <img src={CreditCard} alt="credit-card" className='credit-card' />
-                <img src={DecorationCircle} alt="circle-decor" className='circle-decor' />
+            <div className="flex w-[49%] justify-center items-center relative">
+                <Image src="/credit-card.png" alt="credit-card" width={259} height={250} className='absolute top-[-300px] left-0 z-20 drop-shadow-[-50px_-80px_120px_#1b721b] transition-all duration-500 ease-in w-full' />
+                <Image src="/discover.svg" alt="circle-decor" width={100} height={100} className='absolute right-[-19%] z-10 w-[70%]' />
             </div>
         </div>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Discover from "./Components/Discover";
 import Header from "./Components/Header";
 
 export default function Home() {
   return (
-    <div className="w-[85%] h-screen flex flex-col gap-20">
+    <div className="w-[85%] h-screen flex flex-col gap-40">
       <Header />
+      <Discover />
     </div>
   );
 }
