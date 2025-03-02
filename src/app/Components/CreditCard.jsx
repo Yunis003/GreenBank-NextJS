cffcb556d44c249b68e617d3e19f0998bdb51735
Yunis003 <hi.yunisqulamov@gmail.com>
2025-03-02 23:40:01 +0400

    Credit Card Design and Used

---
+++ src/app/Components/CreditCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './credit-card.css';
+import Image from 'next/image';
+const CreditCard = () => {
+    return (
+        <div className="credit-card" style={{ 
+            // backdropFilter: 'blur(16px)',
+            // backgroundColor: 'rgba(255, 255, 255, 0.1)'
+            background: 'rgba(185, 190, 189, 0.4)',
+            backdropFilter: 'blur(15px)',
+        }}>
+            <div className="credit-card-master">
+                <Image src="/masterc.svg" alt="masterc" width={50} height={50} />
+                <Image src="/nfc.svg" alt='nfc' width={20} height={20}/>
+            </div>
+            <div className="credit-card-informations">
+                <span style={{color:"#B9BEBD"}}>Credit Card No.</span>
+                <span className='card-num' >1234 5678 9101 1121</span>
+                <div className="credit-card-dates">
+                    <div className="name">
+                        <span style={{color:"#B9BEBD"}}>Name</span>
+                        <span>John Doe</span>
+                    </div>
+                    <div className="date">
+                        <span style={{color:"#B9BEBD"}}>Exp.</span>
+                        <span>12/24</span>
+                </div>
+                <Image src="/chip.svg" alt="chip" width={50} height={50} className='chip'/>
+            </div>    
+        </div>
+    </div>
+    );
+}
+
+export default CreditCard;
--- src/app/Components/Discover.jsx
+++ src/app/Components/Discover.jsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import CreditCard from './CreditCard';
+import './credit-card.css';
 
 const Discover = () => {
     return (
@@ -14,11 +16,11 @@ const Discover = () => {
                 <button className='start-btn bg-[#2BB32A] text-white border-none rounded-[4rem] px-8 py-4 flex items-center justify-center gap-4 hover:bg-[#1b721b] cursor-pointer'>Get Started <i className="fa-solid fa-arrow-right"></i></button>
                 <Image src="/users.svg" alt="users" width={300} height={300} />
             </div>  
-            <div className="flex w-[49%] justify-center items-center relative">
-                <Image src="/credit-card.png" alt="credit-card" width={259} height={250} className='absolute top-[-300px] left-0 z-20 drop-shadow-[-50px_-80px_120px_#1b721b] transition-all duration-500 ease-in w-full' />
-                <Image src="/discover.svg" alt="circle-decor" width={100} height={100} className='absolute right-[-19%] z-10 w-[70%]' />
-            </div>
+            <div className="flex w-[49%] justify-center items-center relative card-container">
+                <CreditCard />
+                <Image src="/discover.svg" alt="users" width={420} height={420} className='circle'/>
         </div>
+    </div>
     );
 }
 
