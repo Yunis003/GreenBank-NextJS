0314915c5acd1812d9aca3912c771eaac43ed34f
Yunis003 <hi.yunisqulamov@gmail.com>
2025-03-31 22:32:28 +0400

    Footer Responsive and Fixed Some Bugs

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import Image from "next/image";
 const Applications = () => {
   return (
-    <footer className="flex justify-between items-center gap-[110px] absolute bottom-[-3700px]">
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-3">
+    <footer className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-[110px] bottom-0 left-0 w-full p-5 md:p-10">
+      <div className="flex flex-col gap-5 items-center md:items-start">
+        <div className="flex gap-3 items-center">
           <Image src="/Exclude.svg" width={25} height={25} alt="logo" />
           <p className="text-2xl text-white">GreenBank</p>
         </div>
-        <p className="text-[#ADB2B1] w-[400px]">Discover the power of our secure and rewarding credit cards</p>
+        <p className="text-[#ADB2B1] max-w-[400px] text-center md:text-left">Discover the power of our secure and rewarding credit cards</p>
       </div>
-      <nav className="flex gap-[6.5rem]">
-        <div className="flex flex-col gap-3">
+      <nav className="flex flex-col md:flex-row gap-8 md:gap-[6.5rem] items-center">
+        <div className="flex flex-col gap-3 items-center md:items-start">
           <h3 className="text-xl">About us</h3>
-          <ul>
+          <ul className="flex flex-col gap-1 items-center md:items-start">
             <li className="text-[#ADB2B1]">Investors</li>
             <li className="text-[#ADB2B1]">Features</li>
             <li className="text-[#ADB2B1]">Book a demo</li>
             <li className="text-[#ADB2B1]">Security</li>
           </ul>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 items-center md:items-start">
           <h3 className="text-xl">Products</h3>
-          <ul>
+          <ul className="flex flex-col gap-1 items-center md:items-start">
             <li className="text-[#ADB2B1]">Credit Cards</li>
             <li className="text-[#ADB2B1]">Gift Cards</li>
             <li className="text-[#ADB2B1]">Savings accounts</li>
             <li className="text-[#ADB2B1]">NFT</li>
           </ul>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 items-center md:items-start">
           <h3 className="text-xl">Useful Links</h3>
-          <ul>
+          <ul className="flex flex-col gap-1 items-center md:items-start">
             <li className="text-[#ADB2B1]">Free rewards</li>
             <li className="text-[#ADB2B1]">Documentation</li>
             <li className="text-[#ADB2B1]">Affiliate program</li>
           </ul>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 items-center md:items-start">
           <h3 className="text-xl">Social</h3>
-          <ul>
+          <ul className="flex flex-col gap-1 items-center md:items-start">
             <li className="text-[#ADB2B1]">Changelog</li>
             <li className="text-[#ADB2B1]">Licenses</li>
             <li className="text-[#ADB2B1]">Site Maps</li>
